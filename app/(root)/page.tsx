f42dcec2b1734ac6e5fa24aa1e176fe3ddd5368e
VISHAL SAHOO <bhagis052@gmail.com>
2024-06-06 22:54:13 +0530

    changed page and layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
-import { Button } from '@/components/ui/button'
+
+import PodcastCard from '@/components/PodcastCard'
+import { podcastData } from '@/constants'
 import React from 'react'
 
 const Home = () => {
@@ -6,8 +8,17 @@ const Home = () => {
     <div className="mt-9 flex-col gap-9">Home
     <section className='flex flex-col gap-5'>
     <h1 className='text-20 font-bold text-white-1'>Trending podcasts</h1>
-    <Button className='text-white-1 bg-orange-500'>Button</Button>
-
+    <div className='podcast_grid'>
+    {podcastData.map(({id, title,description , imgURL }) => (
+        <PodcastCard 
+        key={id}
+        imgUrl={imgURL}
+        title={title}
+        description={description}
+        podcastId={id}
+        />
+      ))}
+    </div>
     </section>
     </div>
   )
