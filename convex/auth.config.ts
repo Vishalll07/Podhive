99c380cb38bea8958bcb0b918d15a0dcb58185de
VISHAL SAHOO <bhagis052@gmail.com>
2024-06-11 22:42:11 +0530

    Added authentication

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+
+
+const authConfig = {
+    providers: [
+      {
+        domain: "https://right-platypus-13.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
