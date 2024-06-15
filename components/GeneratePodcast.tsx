6333bcbed02c7f430917fb9d67c71d6c330f45f4
VISHAL SAHOO <bhagis052@gmail.com>
2024-06-15 21:11:18 +0530

    changes15-6-2024

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import { GeneratePodcastProps } from '@/types'
+import React, { useState } from 'react'
+import { Label } from './ui/label'
+import { Textarea } from './ui/textarea'
+import { Button } from './ui/button'
+import { Loader } from 'lucide-react'
+//import { useAction, useMutation } from 'convex/react'
+//import { api } from '@/convex/_generated/api'
+//import { v4 as uuidv4 } from 'uuid';
+import { useToast } from "@/components/ui/use-toast"
 
-const GeneratePodcast = () => {
+//import { useUploadFiles } from '@xixixao/uploadstuff/react';
+
+const useGeneratePodcast = ({
+  setAudio, voiceType, voicePrompt, setAudioStorageId
+}: GeneratePodcastProps) => {
+  const [isGenerating, setIsGenerating] = useState(false);
+  const { toast } = useToast()
+
+
+
+  const generatePodcast = async () => {
+   
+    
+  }
+
+ return { isGenerating, generatePodcast }
+}
+
+const GeneratePodcast = (props: GeneratePodcastProps) => {
+ const { isGenerating, generatePodcast } = useGeneratePodcast(props);
+
   return (
-    <div>GeneratePodcast</div>
+    <div>
+      <div className="flex flex-col gap-2.5">
+        <Label className="text-16 font-bold text-white-1">
+          AI Prompt to generate Podcast
+        </Label>
+        <Textarea 
+          className="input-class font-light focus-visible:ring-offset-orange-1"
+          placeholder='Provide text to generate audio'
+          rows={5}
+          value={props.voicePrompt}
+          onChange={(e) => props.setVoicePrompt(e.target.value)}
+        />
+      </div>
+      <div className="mt-5 w-full max-w-[200px]">
+      <Button type="submit" className="text-16 bg-orange-1 py-4 font-bold text-white-1" onClick={generatePodcast}>
+        {isGenerating ? (
+          <>
+            Generating
+            <Loader size={20} className="animate-spin ml-2" />
+          </>
+        ) : (
+          'Generate'
+        )}
+      </Button>
+      </div>
+      {props.audio && (
+        <audio 
+          controls
+          src={props.audio}
+          autoPlay
+          className="mt-5"
+          onLoadedMetadata={(e) => props.setAudioDuration(e.currentTarget.duration)}
+        />
+      )}
+    </div>
   )
 }
 
